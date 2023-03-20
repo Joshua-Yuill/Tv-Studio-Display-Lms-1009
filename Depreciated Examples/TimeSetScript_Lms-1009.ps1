cd3e1b38da9fa684f5983864d742d0d506c72c00
Joshua <78020975+Joshua-Yuill@users.noreply.github.com>
2023-03-20 22:24:06 +0000

    feat: Added depreciated examples in Poweshell

---
+++ Depreciated Examples/TimeSetScript_Lms-1009.ps1
@@ -0,0 +1,21 @@
+﻿$port= new-Object System.IO.Ports.SerialPort COM3,9600,None,8,one
+$port.open()
+$port.WriteLine("<ID**><MC>")
+Start-Sleep -Milliseconds 50
+$port.WriteLine("<ID01><HR><FU>")
+Start-Sleep -Milliseconds 50
+$port.WriteLine("<ID02><HG><FU>")
+Start-Sleep -Milliseconds 50
+$port.WriteLine("<ID01><M2>")
+Start-Sleep -Milliseconds 50
+$port.WriteLine("<ID02><M1>")
+Start-Sleep -Milliseconds 50
+
+$hour = (Get-Date).Hour
+$min = (Get-Date).Minute
+$sec = (Get-Date).Second
+
+
+$port.WriteLine("<T$($hour)$($min)$($sec)>")
+Start-Sleep -Milliseconds 3
+$port.close()
