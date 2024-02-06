463a9721d5ea98b407266a04d31a64a9d96e4b3f
Joshua-Yuill <78020975+Joshua-Yuill@users.noreply.github.com>
2024-02-06 13:09:08 +0000

    Fixed Bugs

--- Examples/Time_and_NewsHead.py
+++ Examples/Time_and_NewsHead.py
@@ -5,6 +5,10 @@ from datetime import datetime
 
 API_KEY = "1c074bd08729417e8a860b56970af6ae"
 
+API_KEY_WEATHER = "9c54407c094061e37db595e0d4c728be"
+
+NightMode = False
+
 try:
     conn = serial.Serial("COM3", 9600)
 except:
@@ -15,27 +19,40 @@ delay = 0.7
 def displaySetup():
     conn.write("<ID**><MC>\r\n".encode("ascii")) # Setting color mode on both displays
     time.sleep(delay)
-    conn.write("<ID01><HR><FU>\r\n".encode("ascii")) # Setting the color "High Red" and setting it to display time on display ID 1
-    time.sleep(delay)
     conn.write("<ID02><HG><FU>\r\n".encode("ascii")) # Setting the color "High Green" and setting it to display time on display ID 2
     time.sleep(delay)
-    conn.write("<ID01><M2>\r\n".encode("ascii")) # Setting display ID 1 to 24hr time format
+    conn.write("<ID02><HG><M2>\r\n".encode("ascii")) # Setting the color "High Green" and setting it to display time on display ID 2
     time.sleep(delay)
-    conn.write("<ID02><M1>\r\n".encode("ascii")) # Setting display ID 1 to 12hr time format
+    conn.write("<ID01><HG>\r\n".encode("ascii")) # Setting display ID 1 to 12hr time format
     time.sleep(delay)
 
-def nightMode():
-    conn.write("<ID01><L1><PA><TM>\r\n".encode("ascii"))
-    time.sleep(delay)
-    conn.write("<ID02><L1><PA><TM>\r\n".encode("ascii")) 
-    time.sleep(delay)
+def nightMode(timeN):
+    if NightMode == False:
+        conn.write("<ID01><LG>\r\n".encode("ascii"))
+        time.sleep(delay)
+        conn.write("<ID02><LY>\r\n".encode("ascii")) 
+        time.sleep(delay)
+        conn.write("<T{}>\r\n".format(timeN).encode("ascii"))
+    else:
+        conn.write("<T{}>\r\n".format(timeN).encode("ascii"))
 
 def getNews():
+    errored = False
+    Headlines = []
     try:
         response = r.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=" + API_KEY)
+        if errored == True:
+            conn.write("<ID01><HG>\r\n".encode("ascii"))
+            errored = False
+        NResponse = response.json()
+        for i in NResponse['articles']:
+            Headlines.append(i['title'])
     except:
-        response = ("Offline")
-    return response.json()
+        Headlines = []
+        Headlines.append("Offline")
+        conn.write("<ID01><HR>Offline\r\n".encode("ascii"))
+        print('\a')
+    return Headlines
 
 def getTime():
     try:
@@ -46,17 +63,44 @@ def getTime():
         current_time = datetime.now().strftime("%H%M%S")
         return current_time
     
-def displayTime(time):
+def getWeather():
+    try:
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+	        "latitude": 51.4416,
+	        "longitude": 0.1487,
+	        "hourly": "temperature_2m",
+	        "timezone": "Europe/London",
+	        "forecast_days": 1
+        }
+        responses = openmeteo.weather_api(url, params=params)
+        return responses
+    except:
+        return "Offline"
+
+    
+def displayTime(timeN):
     conn.write("<ID02><M2>\r\n".encode("ascii")) # Setting display ID 1 to 24hr time format
     time.sleep(delay)
-    conn.write("<T{}>\r\n".format(time).encode("ascii"))
+    conn.write("<T{}>\r\n".format(timeN).encode("ascii"))
 
-Headlines = []
-NResponse = getNews()
-for i in NResponse['articles']:
-    Headlines.append(i['title'])
+def displayNews(headlines):
+    for i in headlines:
+        conn.write("<ID01><HY>\r\n".encode("ascii"))
+        time.sleep(0.5)
+        conn.write("<ID01><HY>{}\r\n".format(i).replace(u"\u2018", "'").replace(u"\u2019", "'").encode("ascii", "ignore"))
+        time.sleep(8)
 
+def main():
+    displaySetup()
+    while True:
+        if datetime.now().hour >= 22 or datetime.now().hour <= 8:
+            nightMode(getTime())
+            NightMode = True
+            time.sleep(480)
+        else:
+            NightMode = False
+            displayTime(getTime())
+            displayNews(getNews())
 
-print(Headlines[2])
-print(getTime())
-print('\a')
+main()
